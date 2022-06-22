86dd84dc03f4ad429d3059429112a454431964c3
Olga <olga-rovnova@yandex.ru>
2022-06-22 19:42:26 +0300

    task27 file

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите целое положительное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int modulo = n % 10;
+
+while (n > 0)
+{
+    sum = sum + modulo;
+    n = n / 10;
+    modulo = n % 10;
+}
+
+Console.WriteLine("сумма цифр в числе = " + sum);
